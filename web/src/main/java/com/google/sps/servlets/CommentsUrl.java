975ddf8d642ed9b3144a849585ff4aa5d7d6b5bd
thomaspr1205 <60591404+thomaspr1205@users.noreply.github.com>
2021-04-13 06:12:03 -0400

    solve spelling issue (#69)

--- web/src/main/java/com/google/sps/servlets/CommentsUrl.java
+++ web/src/main/java/com/google/sps/servlets/CommentsUrl.java
@@ -42,7 +42,7 @@ public class CommentsUrl extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
 
-    Datastore dataStore = DatastoreOptions.getDefaultInstance().getService();
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
 
     Document redditPage = Jsoup.connect(REDDIT_DISCUSSION_URL).get();
     System.out.printf("Successfully scraped Reddit Page: %s", redditPage.title());
